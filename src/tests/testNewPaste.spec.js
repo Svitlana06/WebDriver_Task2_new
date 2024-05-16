a3cac2b7db28e628d94df10cc9f73e4a0d0f8045
Svitlana Savchuk <svitlanasavchuk06@gmail.com>
2024-05-16 07:58:36 +0300

    changed

--- src/tests/testNewPaste.spec.js
+++ src/tests/testNewPaste.spec.js
@@ -4,43 +4,53 @@ describe('Pastebin site', () => {
         await browser.url('https://pastebin.com');
     })
 
-
-    let codeForPaste = ['git config --global user.name  "New Sheriff in Town"', 'git reset $(git commit-tree HEAD^{tree} -m "Legacy code")', 'git push origin master --force']
+    const codeForPaste = 'git config --global user.name  "New Sheriff in Town" \ngit reset $(git commit-tree HEAD^{tree} -m "Legacy code")\ngit push origin master --force'
 
     it('Adding and checking a new paste', async () => {
 
-        let x = await $('#postform-text');
-        for (let i = 0; i < codeForPaste.length; i++) { // adding a code
-            await x.addValue(codeForPaste[i] + "\n");
-        };
-
-        var selectBoxSyntax = await $('#postform-format')
+        await $('#postform-text').addValue(codeForPaste);
+        
+        const selectBoxSyntax = await $('#postform-format');
         await browser.execute(() => {
             document.getElementById('postform-format').classList.remove('select2-hidden-accessible');
             document.getElementById('postform-format').style = "";
-
         });
-        await selectBoxSyntax.selectByAttribute('value', '8'); // Syntax Highlighting
 
-        var selectBoxExpiration = await $('#postform-expiration')
+        await selectBoxSyntax.selectByAttribute('value', '8');
+
+        const selectBoxExpiration = await $('#postform-expiration');
         await browser.execute(() => {
             document.getElementById('postform-expiration').classList.remove('select2-hidden-accessible');
             document.getElementById('postform-expiration').style = "";
 
         });
-        await selectBoxExpiration.selectByAttribute('value', '10M'); // // adding a paste expiration
+        await selectBoxExpiration.selectByAttribute('value', '10M');
 
-        await $('#postform-name').setValue('how to gain dominance among developers') // adding a paste Name / title
+        await $('#postform-name').setValue('how to gain dominance among developers');
 
-        await $('.-big').click(); })
+        await $('.-big').click();
 
-        it('Checking a new paste', async () => {   
-        
-        await browser.pause(6000);
-        await expect($('.info-top')).toHaveText('how to gain dominance among developers');
-        await expect($('.left').$('a')).toHaveText('Bash');
-        for (let j = 0; j < codeForPaste.length; j++) {
-            await expect($$('.de1')[j]).toHaveText(codeForPaste[j])
+        await browser.waitUntil(async function () {
+            return await $('.info-top > h1')
+        }, {
+            timeout: 50000,
+        })
+
+        const syntaxHighlighting = await $$('a');
+        for (let elem of syntaxHighlighting) {
+            if (elem.getAttribute('href') === "/archive/bash") {
+                await elem.toHaveText('Bash');
+            }
+        }
+        const pasteTitle = await $$('.info-top > h1');
+
+        if (pasteTitle.length === 1) {
+            await expect(pasteTitle[0]).toHaveText('how to gain dominance among developers');
+        }
+        const pasteCode = await $$('.source .bash');
+
+        if (pasteCode.length === 1) {
+            await expect(pasteCode[0]).toHaveText(codeForPaste);
         }
 
     })
